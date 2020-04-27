0aed183b67089ba86db7fe9b2311bc6b1c61fb07
johnmyronmillar <johnmyronmillar@gmail.com>
2020-04-26 23:41:55 -0500

    New  program 3-1

---
+++ Python3-1/Python3_1.py
@@ -0,0 +1,15 @@
+#Capture test scores
+TestScore1 = int(input('Enter first test score: ' ))
+TestScore2 = int(input('Enter second test score: ' ))
+TestScore3 = int(input('Enter third test score: ' ))
+
+#Average test scores
+TestAverage = (TestScore1 + TestScore2 + TestScore3) / 3
+
+#High Score
+HighScore = 90
+
+#Determine if Average is above high score
+if TestAverage >= HighScore:
+    print('Congradulations')
+    print('That\'s a great score')
